8c9cbc776712b914bab570220eda6aa1495de0cc
phanvanhoa2380 <hoagamer2k@gmail.com>
2020-12-18 12:27:16 +0700

    DoAnAnroid

---
+++ app/src/main/java/com/example/lession07_restapi/models/Product.java
@@ -0,0 +1,45 @@
+package com.example.lession07_restapi.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Product {
+    @SerializedName("id")
+    @Expose
+    public Integer id;
+    @SerializedName("name")
+    @Expose
+    public String name;
+    @SerializedName("desc")
+    @Expose
+    public String desc;
+    @SerializedName("image")
+    @Expose
+    public String image;
+    @SerializedName("price")
+    @Expose
+    public int price;
+    @SerializedName("vote")
+    @Expose
+    public Category category;
+    @SerializedName("created_at")
+    @Expose
+    public String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    public String updatedAt;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                ", createdAt='" + createdAt + '\'' +
+                ", updatedAt='" + updatedAt + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/lession07_restapi/responses/BaseResponse.java
@@ -0,0 +1,7 @@
+package com.example.lession07_restapi.responses;
+
+import com.google.gson.annotations.SerializedName;
+
+class BaseResponse {
+
+}
